0501ff771b6556eb63edc9303750e1da1b778df6
Henry Granger <hp.granger98@gmail.com>
2021-11-26 08:43:45 -0300

    tcp

---
+++ seguranca-da-informacao/tcp-udp-server/tcp_client.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+
+def main():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    except socket.error as e:
+        print('The connection failed!')
+        print(f'Error {e}')
+        sys.exit()
+    print('Socket created successfully!')
+
+    HostAim = input('Type the Host or Ip to be connected: ')
+    PortAim = input('Type the Port to be connected: ')
+
+    try:
+        s.connect((HostAim, int(PortAim)))
+        print(f'TCP client  was successfully connected to Host: {HostAim},', f'Port: {PortAim}')
+        s.shutdown(2)
+    except socket.error as e:
+        print(f'Failed to connect to Host: {HostAim},', f'Port: {PortAim}')
+        print(f'Error: {e}')
+        sys.exit()
+
+if __name__ == '__main__':
+    main()
