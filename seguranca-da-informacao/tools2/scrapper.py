bf5614011b906694f3bdc75cc8010b24d4625b29
Henry Granger <hp.granger98@gmail.com>
2021-11-29 14:15:50 -0300

    .

--- seguranca-da-informacao/tools2/scrapper.py
+++ seguranca-da-informacao/tools2/scrapper.py
@@ -1,13 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
-
+'''
 site = requests.get('https://www.ncbi.nlm.nih.gov/gene/85358').content
 soup = BeautifulSoup(site,'html.parser')
 print(soup.prettify())
 '''
-site = requests.get('https://www.climatempo.com.br/').content
+site = requests.get('https://www.ncbi.nlm.nih.gov').content
 soup = BeautifulSoup(site,'html.parser')
-temp = soup.find('span', class_='_block _margin-b-5 -gray')
+#temp = soup.find('span', class_='_block _margin-b-5 -gray')
 
-print(temp.string)
-'''
+print(soup.prettify())
