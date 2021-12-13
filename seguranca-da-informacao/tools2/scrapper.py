6227c9fc09ca957aa933a2f032e21fe9efb5ca42
Henry Granger <hp.granger98@gmail.com>
2021-12-13 15:57:03 -0300

    .

--- seguranca-da-informacao/tools2/scrapper.py
+++ seguranca-da-informacao/tools2/scrapper.py
@@ -1,15 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
-#import os
 
-#os.chdir('D:\\MyProjects\\cursos-dio\\seguranca-da-informacao\\tools1')
-'''
-site = requests.get('https://www.ncbi.nlm.nih.gov/gene/85358').content
-soup = BeautifulSoup(site,'html.parser')
-print(soup.prettify())
-'''
 site = requests.get('https://www.ncbi.nlm.nih.gov').content
 soup = BeautifulSoup(site,'html.parser')
-temp = soup.find('span', class_='street-address')
-
-print(temp.string)
+country = soup.find('span', class_='country-name')
+print(soup.prettify())
+print(country.string)
+print(soup.title.string)
